d84ae03e5c188c56803c7c7f44443700727fd0a9
Renato Granzoto <rgranzot@redhat.com>
2021-10-29 00:24:15 -0300

    Add all tests including removals
    Signed-off-by: Renato Granzoto <rgranzot@redhat.com>

--- test/integration/acceptance/console_endpoints_external_test.go
+++ test/integration/acceptance/console_endpoints_external_test.go
@@ -8,13 +8,15 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"github.com/skupperproject/skupper/client"
 	"github.com/skupperproject/skupper/pkg/utils"
 	"github.com/skupperproject/skupper/test/utils/constants"
 	"gotest.tools/assert"
 	"io"
 	"io/ioutil"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"log"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"strings"
 	"testing"
@@ -28,6 +30,8 @@ var (
 	ADMPASS            = "admin"
 	pubClaimCreated    corev1.Secret
 	pubClaimDownloaded corev1.Secret
+	firstLinkCreated   = ""
+	firstClaimCreated  = ""
 )
 
 type TokenState struct {
@@ -92,7 +96,6 @@ func accessConsole(method string, url string, path string, body io.Reader, user 
 	// If this is a POST
 	if method == "POST" {
 		req.Header.Set("Content-Type", "application/json; charset=utf-8")
-		//req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 	}
 
 	// Define request basic auth
@@ -116,10 +119,6 @@ func accessConsole(method string, url string, path string, body io.Reader, user 
 	bodyResp, err := ioutil.ReadAll(resp.Body)
 
 	strResp := string(bodyResp)
-
-	//fmt.Println("Resp Body => ", strResp)
-	//fmt.Println("Resp Header => ", resp.Header)
-	//fmt.Println("Resp Status => ", resp.StatusCode)
 
 	return strResp, nil
 }
@@ -160,7 +159,7 @@ func createClaimToken(consoleUrl string, minutes int, uses int) (corev1.Secret, 
 
 	var tokenCreated corev1.Secret
 	err = json.Unmarshal([]byte(tokenCreatedStr), &tokenCreated)
-	//fmt.Println("Debug Token Created = ", tokenCreatedStr)
+
 	if err != nil {
 		return corev1.Secret{}, fmt.Errorf("Unable to unmarshal token for %s", consoleUrl)
 	}
@@ -329,6 +328,97 @@ func findNewLink(elementsAfter []LinkStatus, elementsBefore []LinkStatus) (strin
 	}
 }
 
+func getTargets(consoleUrl string) ([]ServiceTarget, error) {
+
+	getPath := fmt.Sprintf("targets")
+
+	targetsStr, err := accessConsole("GET", consoleUrl, getPath, nil, ADMUSER, ADMPASS)
+	if err != nil {
+		return []ServiceTarget{}, fmt.Errorf("Unable to list targets from %s", consoleUrl)
+	}
+
+	var targets []ServiceTarget
+	err = json.Unmarshal([]byte(targetsStr), &targets)
+	if err != nil {
+		return []ServiceTarget{}, fmt.Errorf("Unable to unmarshal targets from %s", consoleUrl)
+	}
+	return targets, nil
+}
+
+func getGenericEndpoint(consoleUrl string, getPath string) (string, error) {
+
+	genEndPointStr, err := accessConsole("GET", consoleUrl, getPath, nil, ADMUSER, ADMPASS)
+	if err != nil {
+		return "", fmt.Errorf("Unable to retrieve endpoint %s", getPath)
+	}
+	return genEndPointStr, nil
+}
+
+// Create the deployment for the Backtend in public namespace
+func CreateBackendDeployment(t *testing.T, cluster *client.VanClient) {
+	name := "hello-world-backend"
+	replicas := int32(1)
+	dep := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: cluster.Namespace,
+			Labels:    map[string]string{"app": name},
+		},
+		Spec: v1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": name,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            name,
+							Image:           "quay.io/skupper/" + name,
+							ImagePullPolicy: corev1.PullIfNotPresent},
+					},
+					RestartPolicy: corev1.RestartPolicyAlways,
+				},
+			},
+		},
+	}
+
+	// Deploying resource
+	dep, err := cluster.KubeClient.AppsV1().Deployments(cluster.Namespace).Create(dep)
+	assert.Assert(t, err)
+}
+
+func delService(consoleUrl string, serviceName string) (error) {
+	_, err := accessConsole("DELETE", consoleUrl, "services/" + serviceName, nil, ADMUSER, ADMPASS)
+	if err != nil {
+		return fmt.Errorf("Unable to delete service %s from %s", serviceName, consoleUrl)
+	}
+	return nil
+}
+
+func delLink(consoleUrl string, linkName string) (error) {
+	_, err := accessConsole("DELETE", consoleUrl, "links/" + linkName, nil, ADMUSER, ADMPASS)
+	if err != nil {
+		return fmt.Errorf("Unable to delete link %s from %s", linkName, consoleUrl)
+	}
+	return nil
+}
+
+func delToken(consoleUrl string, tokenName string) (error) {
+	_, err := accessConsole("DELETE", consoleUrl, "tokens/" + tokenName, nil, ADMUSER, ADMPASS)
+	if err != nil {
+		return fmt.Errorf("Unable to delete claim token %s from %s", tokenName, consoleUrl)
+	}
+	return nil
+}
+
 // Test Token Endpoints
 func testTokensEndpoints(t *testing.T) {
 
@@ -342,6 +432,8 @@ func testTokensEndpoints(t *testing.T) {
 
 	t.Run("test-create-token-in-pub", func(t *testing.T) {
 		pubClaimCreated, err = createClaimToken(PUBCONSOLE, 5, 2)
+		// This will be used latter to test claim removal
+		firstClaimCreated = pubClaimCreated.Name
 		assert.Assert(t, err, "Unable to create a token in Public console")
 		assert.Assert(t, pubClaimCreated.Name != "")
 	})
@@ -383,7 +475,6 @@ func testLinksEndpoints(t *testing.T) {
 	t.Run("test-create-link-in-priv", func(t *testing.T) {
 		err = createLink(PRIVCONSOLE, 4, pubClaimCreated)
 		assert.Assert(t, err, "Unable to create a link in Private")
-		time.Sleep(30 * time.Second)
 	})
 
 	t.Run("test-get-links-after-creation", func(t *testing.T) {
@@ -393,6 +484,9 @@ func testLinksEndpoints(t *testing.T) {
 	})
 
 	newLink, err := findNewLink(linksInPrivAfter, linksInPrivBefore)
+	if err == nil {
+		firstLinkCreated = newLink
+	}
 	assert.Assert(t, err, "Unable to determine the created link name")
 
 	t.Run("test-retrieve-one-link", func(t *testing.T) {
@@ -400,24 +494,21 @@ func testLinksEndpoints(t *testing.T) {
 		ctx, cancelFn := context.WithTimeout(context.Background(), time.Minute)
 		defer cancelFn()
 
-		err = utils.RetryWithContext(ctx, constants.ImagePullingAndResourceCreationTimeout, func() (bool, error) {
-
-			log.Println("[1RG-DEBUG]")
-			t.Log("[2RG-DEBUG]")
-			fmt.Println("[3RG-DEBUG]")
+		err = utils.RetryWithContext(ctx, constants.DefaultTick, func() (bool, error) {
 
 			newLinkData, err = getOneLink(PRIVCONSOLE, newLink)
+			t.Log("[1RG-DEBUG] Err = ", err)
+			t.Log("[2RG-DEBUG] NewLink = ", newLinkData.Connected)
 			if err != nil {
-				log.Println("[4RG-DEBUG] = ", err)
-				t.Log("[5RG-DEBUG] = ", err)
-				fmt.Println("[6RG-DEBUG] = ", err)
+				t.Log("[3RG-DEBUG ERR NOT NIL] Err = ", err)
+				t.Log("[4RG-DEBUG ERR NOT NIL] NewLink = ", newLinkData.Connected)
 				return true, err
 			}
-			log.Println("[7RG-DEBUG] = ", err)
-			t.Log("[8RG-DEBUG] = ", err)
-			fmt.Println("[9RG-DEBUG] = ", err)
+			t.Log("[5RG-DEBUG ERR IS NIL] Err = ", err)
+			t.Log("[6RG-DEBUG ERR IS NIL] NewLink = ", newLinkData.Connected)
 			return newLinkData.Connected, nil
 		})
+
 		assert.Assert(t, err, "Unable to retrieve details about the created link")
 		assert.Assert(t, newLinkData.Name == newLink)
 		assert.Assert(t, newLinkData.Configured)
@@ -434,7 +525,6 @@ func testLinksEndpoints(t *testing.T) {
 	t.Run("test-create-second-link", func(t *testing.T) {
 		err = createLink(PRIVCONSOLE, 3, pubClaimDownloaded)
 		assert.Assert(t, err, "Unable to create a second link in Private")
-		time.Sleep(30 * time.Second)
 	})
 
 	// Adjust link lists
@@ -480,7 +570,6 @@ func testLinksEndpoints(t *testing.T) {
 	t.Run("test-create-third-link", func(t *testing.T) {
 		err = createLink(PRIVCONSOLE, 2, pubClaimDownloaded)
 		assert.Assert(t, err, "Unable to create a third link in Private")
-		time.Sleep(50 * time.Second)
 	})
 
 	t.Run("test-get-links-after-third-creation", func(t *testing.T) {
@@ -505,123 +594,178 @@ func testLinksEndpoints(t *testing.T) {
 // Test Services and Targets
 func testServicesEndpoints(t *testing.T) {
 
-	//var err error
-
 	t.Run("test-get-services-from-priv", func(t *testing.T) {
-		svcsPub, err := getServices(PRIVCONSOLE)
+		svcsPriv, err := getServices(PRIVCONSOLE)
 		assert.Assert(t, err, "Unable to retrieve services from Private")
-		assert.Assert(t, len(svcsPub) > 0)
+		assert.Assert(t, len(svcsPriv) > 0)
 	})
 
-	//
-	////
-	//// +++++ CREATE A SERVICE IN PRIV
-	////
-	//newsvc := ServiceOptions{
-	//	Address:     "hello-world-backend",
-	//	Protocol:    "http",
-	//	Ports:       []int{8080},
-	//	TargetPorts: map[int]int{8080: 8080},
-	//	Labels:      nil,
-	//	Target:      ServiceTarget{
-	//		Name:  "hello-world-backend",
-	//		Type:  "deployment",
-	//		Ports: []PortDescription{
-	//			{ Name: "8080",
-	//				Port: 8080},
-	//		},
-	//	},
-	//}
-	//fmt.Printf("\nCreating service hello-world-backend in PRIV\n============================================\n")
-	//err = createService(PRIVCONSOLE, newsvc)
-	//if err != nil {
-	//	fmt.Println("Unable to create service ", err)
-	//}
-	//
-	////
-	//// +++++ List Services from Pub
-	////
-	//fmt.Printf("\nListing Services in PUB\n============================================\n")
-	//svcsPub, err := getServices(PUBCONSOLE)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, svcPub := range svcsPub {
-	//	if svcPub.Endpoints == nil {
-	//		fmt.Println("Service Exposed through Skupper")
-	//	} else {
-	//		fmt.Println("Service Exposed BY Skupper")
-	//	}
-	//	printService(svcPub)
-	//}
-	//
-	////
-	//// +++++ List Services from Priv
-	////
-	//fmt.Printf("\nListing Services in PRIV\n============================================\n")
-	//svcsPriv, err := getServices(PRIVCONSOLE)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, svcPriv := range svcsPriv {
-	//	if svcPriv.Endpoints == nil {
-	//		fmt.Println("Service Exposed through Skupper")
-	//	} else {
-	//		fmt.Println("Service Exposed BY Skupper")
-	//	}
-	//	printService(svcPriv)
-	//}
-	//
-	////
-	//// +++++ RETRIEVE ONE SPECIFIC SERVICE
-	////
-	//fmt.Printf("\nRetrieve one specific service in PRIV\n============================================\n")
-	//oneService, err := getOneService(PRIVCONSOLE, "hello-world-backend")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//if oneService.Endpoints == nil {
-	//	fmt.Println("Service Exposed through Skupper")
-	//} else {
-	//	fmt.Println("Service Exposed BY Skupper")
-	//}
-	//printService(oneService)
-	//
-	////
-	//// +++++ LIST TARGETS FROM A SERVICE
-	////
-	//fmt.Printf("\nListing Targets in PRIV\n============================================\n")
-	//targetsInSvc, err := getTargets(PRIVCONSOLE)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, target := range targetsInSvc {
-	//	printTargets(target)
-	//}
-	//
-	////
-	//// +++++ SERVICECHECK
-	////
-	//fmt.Printf("\nChecking Service hello-world-backend\n============================================\n")
-	//chkSvc, err := getGenericEndpoint(PRIVCONSOLE, "servicecheck/hello-world-backend")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(chkSvc)
+	// Service test requires a deployment
+	privCluster, err := testRunner.GetPrivateContext(1)
+	assert.Assert(t, err)
+	CreateBackendDeployment(t, privCluster.VanClient)
 
+	t.Run("test-create-service-in-priv", func(t *testing.T) {
+
+		newsvc := ServiceOptions{
+			Address:     "hello-world-backend",
+			Protocol:    "http",
+			Ports:       []int{8080},
+			TargetPorts: map[int]int{8080: 8080},
+			Labels:      nil,
+			Target:      ServiceTarget{
+				Name:  "hello-world-backend",
+				Type:  "deployment",
+				Ports: []PortDescription{
+					{ Name: "8080",
+						Port: 8080},
+				},
+			},
+		}
+		err := createService(PRIVCONSOLE, newsvc)
+		assert.Assert(t, err, "Unable to create service hello-world-backend in Private")
+	})
+
+	t.Run("test-get-services-from-priv", func(t *testing.T) {
+		svcsPriv, err := getServices(PRIVCONSOLE)
+		assert.Assert(t, err, "Unable to retrieve services from Private")
+		foundSvc := false
+		for _, svc := range svcsPriv {
+			if svc.Name == "hello-world-backend" {
+				foundSvc = true
+			}
+		}
+		assert.Assert(t, foundSvc == true)
+	})
+
+	t.Run("test-get-services-from-pub", func(t *testing.T) {
+		svcsPub, err := getServices(PUBCONSOLE)
+		assert.Assert(t, err, "Unable to retrieve services from Public")
+		foundSvc := false
+		for _, svc := range svcsPub {
+			if svc.Name == "hello-world-backend" {
+				foundSvc = true
+			}
+		}
+		assert.Assert(t, foundSvc == true)
+	})
+
+	t.Run("test-get-one-specific-service", func(t *testing.T) {
+
+		var oneService ServiceDefinition
+		var err error
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 3 * time.Minute)
+		defer cancelFn()
+
+		err = utils.RetryWithContext(ctx, constants.DefaultTick, func() (bool, error) {
+			oneService, err = getOneService(PRIVCONSOLE, "hello-world-backend")
+			if err != nil {
+				fmt.Println("[RG-DEBUG] = ", err)
+				return true, err
+			}
+			return oneService.Name == "hello-world-backend" , nil
+		})
+		assert.Assert(t, err, "Unable to retrieve details about service hello-world-backend")
+		assert.Assert(t, oneService.Name == "hello-world-backend")
+	})
+
+	t.Run("test-get-targets-from-service", func(t *testing.T) {
+		var targetsInSvc []ServiceTarget
+		var err error
+
+		ctx, cancelFn := context.WithTimeout(context.Background(), 3 * time.Minute)
+		defer cancelFn()
+
+		err = utils.RetryWithContext(ctx, constants.DefaultTick, func() (bool, error) {
+			targetsInSvc, err = getTargets(PRIVCONSOLE)
+			if err != nil {
+				fmt.Println("[RG-DEBUG] = ", err)
+				return true, err
+			}
+			if len(targetsInSvc) > 0 {
+				return targetsInSvc[0].Name == "hello-world-backend" , nil
+			}
+			return len(targetsInSvc) > 0, nil
+		})
+		assert.Assert(t, err, "Unable to retrieve targets from service hello-world-backend")
+
+		var foundTrgt = false
+		for _, trgt := range targetsInSvc {
+			if trgt.Name == "hello-world-backend" {
+				foundTrgt = true
+			}
+		}
+		assert.Assert(t, foundTrgt == true)
+	})
+
+	t.Run("test-service-check", func(t *testing.T) {
+		chkSvc, err := getGenericEndpoint(PRIVCONSOLE, "servicecheck/hello-world-backend")
+		assert.Assert(t, err, "Unable to check service service hello-world-backend")
+		assert.Assert(t, chkSvc != "")
+	})
 }
 
 func testGeneralEndpoints(t *testing.T) {
 
+	t.Run("test-get-version", func(t *testing.T) {
+		version, err := getGenericEndpoint(PUBCONSOLE, "version")
+		assert.Assert(t, err, "Unable to check Skupper version")
+		assert.Assert(t, version != "")
+	})
+
+	t.Run("test-get-site", func(t *testing.T) {
+		site, err := getGenericEndpoint(PUBCONSOLE, "site")
+		assert.Assert(t, err, "Unable to check Site ID")
+		assert.Assert(t, site != "")
+	})
+
+	t.Run("test-get-events", func(t *testing.T) {
+		events, err := getGenericEndpoint(PUBCONSOLE, "events")
+		assert.Assert(t, err, "Unable to retrieve events")
+		assert.Assert(t, events != "")
+	})
+}
+
+func testRemoveResources( t *testing.T) {
+
+	t.Run("test-remove-service hello-world-private", func(t *testing.T) {
+		err := delService(PRIVCONSOLE, "hello-world-backend")
+		assert.Assert(t, err, "Unable to remove service hello-world-backend")
+	})
+
+	t.Run("test-check-services-removed", func(t *testing.T) {
+		svcsPriv, err := getServices(PRIVCONSOLE)
+		assert.Assert(t, err, "Unable to retrieve services from Private")
+		for _, svc := range svcsPriv {
+			if svc.Name == "hello-world-backend" {
+				t.Log("FAIL : Service hello-world-backend was not removed")
+				t.Fail()
+			}
+		}
+		assert.Assert(t, true)
+	})
+
+	t.Run("test-remove-link", func(t *testing.T) {
+		if firstLinkCreated != "" {
+			err := delLink(PRIVCONSOLE, firstLinkCreated)
+			assert.Assert(t, err, "Unable to remove first link created")
+		} else {
+			t.Skip("Unable to determine the link to be removed. Skipping")
+		}
+	})
+
+	t.Run("test-remove-token", func(t *testing.T) {
+		if firstClaimCreated != "" {
+			err := delToken(PRIVCONSOLE, firstClaimCreated)
+			assert.Assert(t, err, "Unable to remove first token created")
+		} else {
+			t.Skip("Unable to determine the claim token to be removed. Skipping")
+		}
+	})
 }
 
 func TestConsoleEndpointsExternal(t *testing.T) {
-	//ctx, cancelFn := context.WithTimeout(context.Background(), constants.ImagePullingAndResourceCreationTimeout)
-	//defer cancelFn()
-
-	// Setup deployments
-	//defer console.TearDown(t, testRunner)
-	//console.Setup(ctx, t, testRunner)
 
 	// Get context for Public
 	pubCluster, err := testRunner.GetPublicContext(1)
@@ -653,4 +797,7 @@ func TestConsoleEndpointsExternal(t *testing.T) {
 
 	// Test General Endpoints
 	testGeneralEndpoints(t)
+
+	// Test Removing services
+	testRemoveResources(t)
 }
